1d162af5e149ebee0fe78d299c017727d0d91689
viniciushenrique <viniciushenrique02@hotmail.com>
2023-05-21 05:36:17 -0300

    atividade

---
+++ atividade1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numero1, numero2, soma;
+        System.out.println("Imforme os valores da soma: ");
+        numero1 = input.nextInt();
+        numero2 = input.nextInt();
+        soma = numero1 + numero2;
+        System.out.println("Soma = " + soma);
+    }
+}
+    
+
